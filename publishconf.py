4380626420ede91742508fc80549d6583e0905d5
Jake VanderPlas <jakevdp@gmail.com>
2017-12-21 06:06:17 -0800

    Fix RSS URL

--- publishconf.py
+++ publishconf.py
@@ -14,8 +14,8 @@ SITEURL = 'http://jakevdp.github.io'
 RELATIVE_URLS = False
 
 SHOW_FEED = True
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+FEED_ALL_ATOM = '/feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = '/feeds/%s.atom.xml'
 FEED_USE_SUMMARY = True  # from the feed_summary plugin
 
 DELETE_OUTPUT_DIRECTORY = True
